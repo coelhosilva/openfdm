593216cdcf362aa0bfa7ee667d2386a534e463e2
coelhosilva <lucascoelhosilva@gmail.com>
2024-06-14 12:53:37 -0300

    openfdm.processor: added the GoogleCloudStorageFlightLoader connector.

--- openfdm/processor/connectors/flight_loader/__init__.py
+++ openfdm/processor/connectors/flight_loader/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "FlightLoader",
     "SampleFlightLoader",
+    "GoogleCloudStorageFlightLoader",
 ]
 
 from .base import FlightLoader
 from .sample_loader import SampleFlightLoader
+from .gcs_loader import GoogleCloudStorageFlightLoader
