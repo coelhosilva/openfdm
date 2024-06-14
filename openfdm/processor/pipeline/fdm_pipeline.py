9d0ae67576064ebf8e1a452f03a7830a3c39bcb7
coelhosilva <lucascoelhosilva@gmail.com>
2024-06-14 19:04:46 -0300

    updated the FlightDataMonitoringPipeline to support maximum runtime checking and batch processing.

--- openfdm/processor/pipeline/fdm_pipeline.py
+++ openfdm/processor/pipeline/fdm_pipeline.py
@@ -3,6 +3,7 @@ __all__ = [
 ]
 
 import logging
+from time import perf_counter
 from tqdm import tqdm
 from openfdm.rule_engine.safety import (
     FlightDataMonitoringEvent,
@@ -24,10 +25,14 @@ class FlightDataMonitoringPipeline:
         flight_loader: FlightLoader,
         fdm_events: list[FlightDataMonitoringEvent],
         output_connector: OutputConnector,
+        maximum_run_time_seconds: int = None,
+        batch_size: int = None,
     ):
         self.flight_loader = flight_loader
         self.fdm_events = fdm_events
         self.output_connector = output_connector
+        self.maximum_run_time_seconds = maximum_run_time_seconds
+        self.batch_size = batch_size
 
     def transform_flight(
         self,
@@ -36,34 +41,48 @@ class FlightDataMonitoringPipeline:
         return [event(flight) for event in self.fdm_events]
 
     def run(self) -> None:
+        start_time = perf_counter()
         logger.log(
             level=logging.INFO,
             msg="[openfdm-pipeline] Starting the Flight Data Monitoring pipeline.",
         )
-        processed_events = []
+        for batch_ix, flight_batch in enumerate(
+            self.flight_loader.load_flight_batch(batch_size=self.batch_size)
+        ):
+            logger.log(
+                level=logging.INFO,
+                msg=f"[openfdm-pipeline] Processing batch {batch_ix}.",
+            )
+            processed_events = []
+            for flight in tqdm(flight_batch):
+                try:
+                    logger.log(
+                        level=logging.INFO, msg=f"Processing flight {flight.flight_id}."
+                    )
+                    processed_events.extend(self.transform_flight(flight))
+                except Exception as e:
+                    logger.error(f"Error processing flight: {repr(e)}")
 
-        for flight in tqdm(self.flight_loader.load_flights()):
-            try:
-                logger.log(
-                    level=logging.INFO, msg=f"Processing flight {flight.flight_id}."
-                )
-                processed_events.extend(self.transform_flight(flight))
-            except Exception as e:
-                logger.error(f"Error processing flight: {repr(e)}")
+            if self.output_connector.single_output_connector:
+                for event in processed_events:
+                    self.output_connector.process_fdm_event_output(event)
+            else:
+                self.output_connector.process_fdm_event_output(processed_events)
 
-        if self.output_connector.single_output_connector:
-            for event in processed_events:
-                self.output_connector.process_fdm_event_output(event)
-        else:
-            self.output_connector.process_fdm_event_output(processed_events)
-
-        # add aftermath here of removals and so forth. try exceptions.
-        # for event in processed_events:
-        logger.log(
-            level=logging.INFO,
-            msg="[openfdm-pipeline] Post processing events...",
-        )
-        self.flight_loader.post_process_events(processed_events)
+            # add aftermath here of removals and so forth. try exceptions.
+            # for event in processed_events:
+            logger.log(
+                level=logging.INFO,
+                msg="[openfdm-pipeline] Post processing events...",
+            )
+            self.flight_loader.post_process_events(processed_events)
+            if self.maximum_run_time_seconds is not None:
+                if perf_counter() - start_time > self.maximum_run_time_seconds:
+                    logger.log(
+                        level=logging.INFO,
+                        msg="[openfdm-pipeline] Maximum run time reached. Exiting...",
+                    )
+                    break
 
         logger.log(
             level=logging.INFO,
