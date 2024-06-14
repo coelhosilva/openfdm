a45741d8286b668d4e36dcf9bcba82acc91c5451
coelhosilva <lucascoelhosilva@gmail.com>
2024-06-14 12:54:49 -0300

    openfdm.processor.pipeline: improved logging. Added exception handling.

--- openfdm/processor/pipeline/fdm_pipeline.py
+++ openfdm/processor/pipeline/fdm_pipeline.py
@@ -2,6 +2,8 @@ __all__ = [
     "FlightDataMonitoringPipeline",
 ]
 
+import logging
+from tqdm import tqdm
 from openfdm.rule_engine.safety import (
     FlightDataMonitoringEvent,
     FlightDataMonitoringEventOutput,
@@ -11,6 +13,9 @@ from openfdm.dataframes import (
 )
 from openfdm.processor.connectors.flight_loader import FlightLoader
 from openfdm.processor.connectors.output_connector import OutputConnector
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class FlightDataMonitoringPipeline:
@@ -29,17 +34,33 @@ class FlightDataMonitoringPipeline:
         flight: StandardizedFlightDataframe,
     ) -> list[FlightDataMonitoringEventOutput]:
         return [event(flight) for event in self.fdm_events]
-        # ) -> dict[str, FOQAEventOutput]:
-        # return {event.event_name: event(flight) for event in self.foqa_events}
 
     def run(self) -> None:
+        logger.log(
+            level=logging.INFO, msg="Starting the Flight Data Monitoring pipeline."
+        )
         processed_events = []
 
-        for flight in self.flight_loader.load_flights():
-            processed_events.extend(self.transform_flight(flight))
+        for flight in tqdm(self.flight_loader.load_flights()):
+            try:
+                logger.log(
+                    level=logging.INFO, msg=f"Processing flight {flight.flight_id}."
+                )
+                processed_events.extend(self.transform_flight(flight))
+            except Exception as e:
+                logger.error(f"Error processing flight: {repr(e)}")
 
         if self.output_connector.single_output_connector:
             for event in processed_events:
                 self.output_connector.process_fdm_event_output(event)
         else:
             self.output_connector.process_fdm_event_output(processed_events)
+
+        # add aftermath here of removals and so forth. try exceptions.
+        # for event in processed_events:
+        self.flight_loader.post_process_events(processed_events)
+
+        logger.log(
+            level=logging.INFO,
+            msg="Flight Data Monitoring pipeline finished.",
+        )
